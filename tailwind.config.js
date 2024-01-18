ea1c7c7b2bb66b833f3eedddd19f43a1fa1fbb19
arpitmoliya <arpitmoliya153@gmail.com>
2024-01-18 15:57:15 +0530

    feat: add px size of width, height and margin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx}'],
+  darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        1280: '1280px',
+        600: '600px',
+      },
+      height: {
+        832: '832px',
+      },
+      margin: {
+        340: '340px',
+        940: '940px',
+        710: '710px',
+        90: '90px',
+        122: '122px',
+      },
+    },
   },
   plugins: [],
 }
