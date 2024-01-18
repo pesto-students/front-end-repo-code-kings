4f2560c5ed2f56b9985fb69697ad4c3003369780
arpitmoliya <arpitmoliya153@gmail.com>
2024-01-18 21:07:30 +0530

    feat: create base frame layout for web page

--- src/pages/components/Baseframe.jsx
+++ src/pages/components/Baseframe.jsx
@@ -1,10 +1,11 @@
-const BaseFrameLayout = () => {
+const BaseFrameLayout = ({ children }) => {
   return (
     <div className="flex ">
       <div className="w-0.5 h-832 bg-neutral-500 absolute ml-340"></div>
       <div className="w-600 h-0.5 bg-neutral-500 absolute ml-340 mt-90"></div>
       <div className="w-600 h-0.5 bg-neutral-500 absolute ml-340 mt-710"></div>
       <div className="w-0.5 h-832 bg-neutral-500 absolute ml-940"></div>
+      {children}
     </div>
   )
 }
