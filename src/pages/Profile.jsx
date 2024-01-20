98a3a933bb9bac0c2f25d4963a31be8ff1474605
arpitmoliya <arpitmoliya153@gmail.com>
2024-01-20 14:33:31 +0530

    feat: create basic page for routing

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import BaseFrameLayout from './components/Baseframe'
+import NavigationMenu from './components/NavigationMenu'
+
+const Profile = () => {
+  return (
+    <BaseFrameLayout>
+      <NavigationMenu></NavigationMenu>
+    </BaseFrameLayout>
+  )
+}
+
+export default Profile
---
+++ src/pages/Settings.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import BaseFrameLayout from './components/Baseframe'
+import NavigationMenu from './components/NavigationMenu'
+
+const Settings = () => {
+  return (
+    <BaseFrameLayout>
+      <NavigationMenu></NavigationMenu>
+    </BaseFrameLayout>
+  )
+}
+
+export default Settings
