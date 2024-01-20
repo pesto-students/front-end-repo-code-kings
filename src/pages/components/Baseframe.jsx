10bfba8e29b278eea7bb986de14753bafbab9fda
arpitmoliya <arpitmoliya153@gmail.com>
2024-01-20 14:31:36 +0530

    add conditional rendering for base frame

--- src/pages/components/Baseframe.jsx
+++ src/pages/components/Baseframe.jsx
@@ -1,9 +1,20 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom'
+
 const BaseFrameLayout = ({ children }) => {
+  const location = useLocation()
+
+  const isExercisesRoute = location.pathname === '/exercises'
+
   return (
     <div className="flex ">
       <div className="w-0.5 h-832 bg-neutral-500 absolute ml-340"></div>
       <div className="w-600 h-0.5 bg-neutral-500 absolute ml-340 mt-90"></div>
-      <div className="w-600 h-0.5 bg-neutral-500 absolute ml-340 mt-710"></div>
+      {isExercisesRoute ? (
+        <div></div>
+      ) : (
+        <div className="w-600 h-0.5 bg-neutral-500 absolute ml-340 mt-710"></div>
+      )}
       <div className="w-0.5 h-832 bg-neutral-500 absolute ml-940"></div>
       {children}
     </div>
