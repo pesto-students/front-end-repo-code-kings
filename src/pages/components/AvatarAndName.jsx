176a2ae87f6b16b6f535a710ee1d21c970ee31b3
arpitmoliya <arpitmoliya153@gmail.com>
2024-01-23 15:45:49 +0530

    feat: add click event to navigate to profile page

--- src/pages/components/AvatarAndName.jsx
+++ src/pages/components/AvatarAndName.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const AvatarAndName = () => {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate('/profile')
+  }
+
   return (
-    <div className="max-sm:mr-[2%] max-sm:right-[1%] flex absolute md:right-[21%] top-[1%] items-center w-fit gap-5">
+    <div
+      className="max-sm:mr-[2%] max-sm:right-[1%] flex absolute md:right-[21%] top-[1%] items-center w-fit gap-5 cursor-pointer"
+      onClick={handleClick}
+    >
       <div className="max-min:hidden lg:text-2xl">Profile Name</div>
       <div className="avatar w-[50px]  h-[50px] rounded-full  overflow-hidden border-white">
         <img
