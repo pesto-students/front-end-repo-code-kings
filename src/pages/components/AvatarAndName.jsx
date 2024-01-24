d685d18e4120fb69e00379e04fd53ea45751b0fe
arpitmoliya <arpitmoliya153@gmail.com>
2024-01-24 11:05:03 +0530

    feat: add condition for rendering

--- src/pages/components/AvatarAndName.jsx
+++ src/pages/components/AvatarAndName.jsx
@@ -1,27 +1,49 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 
 const AvatarAndName = () => {
   const navigate = useNavigate()
+
+  const location = useLocation()
+
+  const isNewRoutineRoute = location.pathname === '/newRoutine'
 
   const handleClick = () => {
     navigate('/profile')
   }
 
   return (
-    <div
-      className="max-sm:mr-[2%] max-sm:right-[1%] flex absolute md:right-[21%] top-[1%] items-center w-fit gap-5 cursor-pointer"
-      onClick={handleClick}
-    >
-      <div className="max-min:hidden lg:text-2xl">Profile Name</div>
-      <div className="avatar w-[50px]  h-[50px] rounded-full  overflow-hidden border-white">
-        <img
-          src="/pimage.jpg"
-          className="h-[100%] w-[100%] object-cover"
-          alt="profile logo"
-        />
-      </div>
-    </div>
+    <>
+      {isNewRoutineRoute ? (
+        <div
+          className="max-sm:mr-[2%] max-sm:right-[1%] flex absolute md:right-[41%] top-[1%] items-center w-fit gap-5 cursor-pointer"
+          onClick={handleClick}
+        >
+          <div className="max-min:hidden lg:text-2xl">Profile Name</div>
+          <div className="avatar w-[50px]  h-[50px] rounded-full  overflow-hidden border-white">
+            <img
+              src="/pimage.jpg"
+              className="h-[100%] w-[100%] object-cover"
+              alt="profile logo"
+            />
+          </div>
+        </div>
+      ) : (
+        <div
+          className="max-sm:mr-[2%] max-sm:right-[1%] flex absolute md:right-[21%] top-[1%] items-center w-fit gap-5 cursor-pointer"
+          onClick={handleClick}
+        >
+          <div className="max-min:hidden lg:text-2xl">Profile Name</div>
+          <div className="avatar w-[50px]  h-[50px] rounded-full  overflow-hidden border-white">
+            <img
+              src="/pimage.jpg"
+              className="h-[100%] w-[100%] object-cover"
+              alt="profile logo"
+            />
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
