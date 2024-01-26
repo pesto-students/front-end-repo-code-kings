521cef1d337d661b6e28d79f7e15420afb364c9e
arpitmoliya <arpitmoliya153@gmail.com>
2024-01-26 14:47:52 +0530

    feat: add function for changing elements and style when click happen

--- src/pages/NewRoutineWithExerciseList.jsx
+++ src/pages/NewRoutineWithExerciseList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BaseFrameLayout from './components/Baseframe'
 import NavigationMenu from './components/NavigationMenu'
 import RoutineMenu from './components/RoutineMenu'
@@ -6,6 +6,11 @@ import SearchBar from './components/SearchBar'
 import ExerciseBlock from './components/ExerciseBlock'
 
 const NewRoutineWithExerciseList = () => {
+  const [isItemSelected, setSelectedItem] = useState(null)
+
+  const handleIsItemSelected = (data) => {
+    setSelectedItem(data)
+  }
   return (
     <BaseFrameLayout>
       <NavigationMenu />
@@ -16,11 +21,11 @@ const NewRoutineWithExerciseList = () => {
         </h1>
         <div className="h-0.5 bg-neutral-500 fixed top-[8%]  w-full"></div>
       </div>
-      <div className=" text-white ml-[60%] mt-[4%] text-center flex flex-col items-center gap-10 ">
+      <div className=" text-white ml-[60%] mt-[4%] pt-[0.5%] text-center flex flex-col items-center gap-10 overflow-hidden">
         <SearchBar />
         <div className="w-[90%] max-h-[565px] no-scrollbar overflow-y-auto text-center">
-          <ExerciseBlock />
-          <ExerciseBlock />
+          <ExerciseBlock onData={handleIsItemSelected} />
+          <ExerciseBlock onData={handleIsItemSelected} />
           <ExerciseBlock />
           <ExerciseBlock />
           <ExerciseBlock />
@@ -28,11 +33,13 @@ const NewRoutineWithExerciseList = () => {
           <ExerciseBlock />
           <ExerciseBlock />
         </div>
-        <div className="w-[90%]">
-          <button className="bg-blue-500 text-xl h-[45px] w-full">
-            Add Exercises
-          </button>
-        </div>
+        {!isItemSelected && (
+          <div className="w-[90%]">
+            <button className="bg-blue-500 text-xl h-[45px] w-full">
+              Add Exercises
+            </button>
+          </div>
+        )}
       </div>
     </BaseFrameLayout>
   )
