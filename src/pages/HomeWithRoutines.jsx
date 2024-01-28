419ddddb7d724fa7134411d8eccfba2b0921c607
arpitmoliya <arpitmoliya153@gmail.com>
2024-01-29 02:13:45 +0530

    fix: make height of main frame fixed

--- src/pages/HomeWithRoutines.jsx
+++ src/pages/HomeWithRoutines.jsx
@@ -12,7 +12,7 @@ const HomeWithRoutines = () => {
       <div className="text-white fixed w-[54.5%] max-h-screen overflow-hidden  left-[25.25%] right-[20.25%] top-[54%]">
         <div className="text-3xl pl-[40px] py-6">My Routines</div>
         <div className="items-center">
-          <div className="w-[71.79%] mx-auto min-h-[310px] overflow-y-scroll no-scrollbar">
+          <div className="w-[71.79%] mx-auto h-[310px] overflow-y-scroll no-scrollbar">
             <RoutineBlock />
             <RoutineBlock />
             <RoutineBlock />
