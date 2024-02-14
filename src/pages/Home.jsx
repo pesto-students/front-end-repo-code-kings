da54812893beb2cc870731c8927d059616084fbf
arpitmoliya <arpitmoliya153@gmail.com>
2024-02-14 22:25:17 +0530

    fix: remove code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,13 @@
-import { useState, useEffect } from 'react'
+import { Cookies } from 'react-cookie'
+import { useNavigate } from 'react-router-dom'
 import BaseFrameLayout from './components/Baseframe'
 import NavigationMenu from './components/NavigationMenu'
 import RoutineMenu from './components/RoutineMenu'
-import { useNavigate } from 'react-router-dom'
-import { Cookies } from 'react-cookie'
 
 const Home = () => {
   const navigate = useNavigate()
-  const [isLogin, setIsLogin] = useState(true)
   const cookie = new Cookies()
-  useEffect(() => {
-    if (cookie.get('jwt')) {
-      setIsLogin(true)
-    } else {
-      setIsLogin(false)
-    }
-  }, [])
+  const token = cookie.get('jwt')
 
   const handleSignIn = () => {
     navigate('/signin')
@@ -28,7 +20,7 @@ const Home = () => {
     <BaseFrameLayout>
       <NavigationMenu />
       <RoutineMenu />
-      {!isLogin && (
+      {!token && (
         <div className="text-white flex gap-5   absolute md:right-[21%] top-[2%] ">
           <button
             className="px-4 py-1 bg-blue-500 rounded-md cursor-pointer hover:bg-blue-400"
